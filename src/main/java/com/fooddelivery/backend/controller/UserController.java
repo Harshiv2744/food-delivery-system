ed2e61973d27b318445d4b107db4b11b3699c1bd
Harshiv2744 <22dit048@charusat.edu.in>
2026-02-17 22:59:24 +0530

    Day 3 - Added repository, service and basic CRUD

---
+++ src/main/java/com/fooddelivery/backend/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.fooddelivery.backend.controller;
+
+import com.fooddelivery.backend.model.User;
+import com.fooddelivery.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        user.setCreatedAt(LocalDateTime.now());
+        return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+}
